faed38af61b56864026eb4d5501ebb7bd30ab5ff
opogorilyi <olek.pogorilyi@gmail.com>
2025-04-25 17:07:57 +1000

    create jest.config.js

--- api-fetcher/jest.config.js
+++ api-fetcher/jest.config.js
@@ -1,8 +1,4 @@
 export default {
     // Using Node.js with ESM
     testEnvironment: 'node',
-    // Transform JavaScript files with Babel to handle ESM
-    transform: {
-      '^.+\\.js$': 'babel-jest',
-    },
 };
